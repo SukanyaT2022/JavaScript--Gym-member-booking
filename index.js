a6e562ea574d2aa500179836057644defa8a95b5
SukanyaT2022 <sukanyat2022a@gmail.com>
2024-12-03 09:57:34 -0500

    add

---
+++ index.js
@@ -0,0 +1,20 @@
+//step 1 fetch get the value 
+
+let memberName = " "
+let memberGender = " "
+let memberAge = " "
+let memberEmail = " "
+let memberPhone = " "
+let typeMembership = " "
+let gymClassName = " "
+
+function memberInfoFunc(){
+   memberName = document.getElementById('memberNameID').value
+   memberGender = document.memberForm.gender.value
+   memberAge = document.getElementById('ageID').value
+   memberEmail = document.getElementById('emailID').value
+   memberPhone = document.getElementById('phoneID').value
+   typeMembership = document.memberForm.memberType.value
+   gymClassName = document.memberForm.gymClassName.value
+}
+
