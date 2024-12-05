8bb51f2ddaa5f2f33c701e77bc7de21747b5a4c4
SukanyaT2022 <sukanyat2022a@gmail.com>
2024-12-05 09:57:55 -0500

    add

--- index.js
+++ index.js
@@ -8,13 +8,27 @@ let memberPhone = " "
 let typeMembership = " "
 let gymClassName = " "
 
+let totalCost = 0
+
+
 function memberInfoFunc(){
+   console.log('hi')
    memberName = document.getElementById('memberNameID').value
    memberGender = document.memberForm.gender.value
    memberAge = document.getElementById('ageID').value
    memberEmail = document.getElementById('emailID').value
    memberPhone = document.getElementById('phoneID').value
    typeMembership = document.memberForm.memberType.value
-   gymClassName = document.memberForm.gymClassName.value
+   gymClassName = document.memberForm.gymClassName.values
+console.log(gymClassName)
+   totalCost = parseInt(typeMembership)
+
+   for (i=0;i<gymClassName.length;i++){
+totalCost += parseInt(gymClassName[i])
+   }
+   console.log(totalCost)
 }
 
+function memberTypeCost(){
+
+}
