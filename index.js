24eafa82a69edf9642703b5392830694c8d75bb7
SukanyaT2022 <sukanyat2022a@gmail.com>
2024-12-10 10:05:25 -0500

    add

--- index.js
+++ index.js
@@ -7,28 +7,46 @@ let memberEmail = " "
 let memberPhone = " "
 let typeMembership = " "
 let gymClassName = " "
-
 let totalCost = 0
 
 
-function memberInfoFunc(){
-   console.log('hi')
-   memberName = document.getElementById('memberNameID').value
-   memberGender = document.memberForm.gender.value
-   memberAge = document.getElementById('ageID').value
-   memberEmail = document.getElementById('emailID').value
-   memberPhone = document.getElementById('phoneID').value
-   typeMembership = document.memberForm.memberType.value
-   gymClassName = document.memberForm.gymClassName.values
-console.log(gymClassName)
-   totalCost = parseInt(typeMembership)
 
+
+
+function memberInfoFunc(){
+memberName = document.getElementById('memberNameID').value
+memberGender = document.memberForm.gender.value
+memberAge = document.getElementById('ageID').value
+memberEmail = document.getElementById('emailID').value
+memberPhone = document.getElementById('phoneID').value
+typeMembership = document.memberForm.memberType.value
+gymClassName = document.memberForm.gymClassName.values
+totalCost = parseInt(typeMembership)
+   validateInputboxFill()
    for (i=0;i<gymClassName.length;i++){
 totalCost += parseInt(gymClassName[i])
    }
    console.log(totalCost)
+   
 }
 
 function memberTypeCost(){
 
 }
+function validateInputboxFill(){
+memberName = document.getElementById('memberNameID').value
+memberEmail = document.getElementById('emailID').value
+memberPhone = document.getElementById('phoneID').value
+
+   if(memberName == ""){
+document.getElementById('memberNamePtagID').innerHTML = "Member Name" + "<font color = red> &nbsp * Member Name is missing.</font> "
+   }
+   if(memberEmail == ""){
+      document.getElementById('memberEmailPtagID').innerHTML = "Member Email" + "<font color = red> &nbsp * Member email is missing.</font> "
+   }
+   
+         if(memberPhone == ""){
+           document.getElementById('memberPhonePtagID').innerHTML = "Member Phone No." + "<font color = red> &nbsp *  Member phone number is missing.</font> "
+               }
+            }      
+
