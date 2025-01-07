ad72aaec2d5c32ffadb5e689e001bb841cbceac0
SukanyaT2022 <sukanyat2022a@gmail.com>
2025-01-07 10:05:56 -0500

    add

--- index.js
+++ index.js
@@ -18,27 +18,33 @@ function memberInfoFunc() {
 
   //line 20 read the value = "" type of memeber ship type and price
   typeMembership = document.memberForm.memberType.value;
-  var position = typeMembership.indexOf(':')
-  var membershipName = typeMembership.substring(0,position)
-  var membershipCost = typeMembership.substring(position+1)
+  var position = typeMembership.indexOf(":");
+  var membershipName = typeMembership.substring(0, position);
+  var membershipCost = typeMembership.substring(position + 1);
   gymClassName = document.memberForm.gymClassName;
 
   // we continue to do make check box class read show on console
+  //extract name and cost
+  var captureClass = "";
+  for (i = 0; i < gymClassName.length; i++) {
+    if (gymClassName[i].checked == true) {
+      position = gymClassName[i].value.indexOf(":");//use : to divide
+      classNameType = gymClassName[i].value.substring(0, position);//read before :
+      membershipCost = gymClassName[i].value.substring(position + 1);//read after:
+      totalCost = totalCost + parseInt(membershipCost);
+      captureClass = captureClass + classNameType + ", ";
 
-  for (i=0; i<gymClassName.length; i++){
-   if(gymClassName[i].checked == true){
-      totalCost = totalCost + parseInt(gymClassName[i].value)
-   
-   }
+    }
   }
-
-
+captureClass = captureClass.substring(0,captureClass.length-2)
   totalCost += parseInt(membershipCost);
   validateInputboxFill();
   console.log(totalCost);
 
-document.getElementById('finalCost').innerText = "Final Cost: " + totalCost;
-document.getElementById('memberTypeID').innerText = " Membership Type: " + membershipName;
+  document.getElementById("finalCost").innerText = "Final Cost: " + totalCost;
+  document.getElementById("memberTypeID").innerHTML =
+    " Membership Type: " + membershipName + "<br>" + "Class Type: " + captureClass;
+
 
   // after click submit the input box blank
   document.getElementById("ageID").value = " ";
@@ -58,19 +64,9 @@ document.getElementById('memberTypeID').innerText = " Membership Type: " + membe
   document.getElementById("goldMemberID").checked = false;
   document.getElementById("silverMemberID").checked = false;
   document.getElementById("greenMembeID").checked = false;
-
 }
 
-function memberTypeCost() {
-
-
-
-
-
-
-
-}
-
+function memberTypeCost() {}
 
 function validateInputboxFill() {
   memberName = document.getElementById("memberNameID").value;
