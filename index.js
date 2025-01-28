c468aa69eaf3b91fc24a5dc7d655b1eeea92d38e
SukanyaT2022 <sukanyat2022a@gmail.com>
2025-01-28 09:56:13 -0500

    add

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ let typeMembership = " ";
 let gymClassName = " ";
 let totalCost = 0;
 
+// submit button claculate entire calculation
 function memberInfoFunc() {
+  totalCost = 0
   memberName = document.getElementById("memberNameID").value;
   memberGender = document.memberForm.gender.value;
   memberAge = document.getElementById("ageID").value;
@@ -27,18 +29,19 @@ monthlyYearly = document.memberForm.subcribeMY.value;
 
   // we continue to do make check box class read show on console
   //extract name and cost
-  var captureClass = "";
+  var captureClass = "<table width=40%>";
   for (i = 0; i < gymClassName.length; i++) {
     if (gymClassName[i].checked == true) {
       position = gymClassName[i].value.indexOf(":");//use : to divide
       classNameType = gymClassName[i].value.substring(0, position);//read before :
       membershipCost = gymClassName[i].value.substring(position + 1);//read after:
       totalCost = totalCost + parseInt(membershipCost);
-      captureClass = captureClass + classNameType + " - $" + membershipCost + "<br>";
+      captureClass = captureClass + "<tr><td>" + classNameType + " </td><td> $" + membershipCost + "</td></tr>";
 
     }
   }
-captureClass = captureClass.substring(0,captureClass.length-2)
+
+// captureClass = captureClass.substring(0,captureClass.length-2)
 if(monthlyYearly == 'monthly'){
   totalCost += parseInt(membershipCost); 
 }
@@ -53,30 +56,33 @@ if (monthlyYearly == "yearly"){
   // totalCost = totalCost - totalCost * 0.2
 }
 var taxCost = totalCost + totalCost * 0.12
+captureClass += "<tr><td> Final Cost</td><td> $" +  totalCost + "</td></tr>"
+captureClass += "<tr><td> Final Cost after 12% tax</td><td> $" +  taxCost + "</td></tr>"
+captureClass += "</table>"
+  // document.getElementById("finalCost").innerHTML = "Final Cost: $" + totalCost + "<br> Final cost after 12% tax: $" + taxCost;
 
-  document.getElementById("finalCost").innerHTML = "Final Cost: $" + totalCost + "<br> Final cost after 12% tax: $" + taxCost;
   document.getElementById("memberTypeID").innerHTML =
     " Membership Type: " + membershipName + " ($" + membershipCost + " ) monthly and ($"+ parseInt(membershipCost) * 12 + ") yearly <br>" + "<b>Class Type: </b><br>" + captureClass;
 
 
   // after click submit the input box blank
-  document.getElementById("ageID").value = " ";
-  document.getElementById("emailID").value = " ";
-  document.getElementById("phoneID").value = " ";
-  document.getElementById("memberNameID").value = " ";
+  // document.getElementById("ageID").value = " ";
+  // document.getElementById("emailID").value = " ";
+  // document.getElementById("phoneID").value = " ";
+  // document.getElementById("memberNameID").value = " ";
   // after click submit radio button blank
-  document.getElementById("maleID").checked = false;
-  document.getElementById("femaleID").checked = false;
+  // document.getElementById("maleID").checked = false;
+  // document.getElementById("femaleID").checked = false;
 
-  document.getElementById("yogaClassID").checked = false;
-  document.getElementById("boxingClassID").checked = false;
-  document.getElementById("pilatesClassID").checked = false;
-  document.getElementById("intenseClassID").checked = false;
-  document.getElementById("cyclingClassID").checked = false;
+  // document.getElementById("yogaClassID").checked = false;
+  // document.getElementById("boxingClassID").checked = false;
+  // document.getElementById("pilatesClassID").checked = false;
+  // document.getElementById("intenseClassID").checked = false;
+  // document.getElementById("cyclingClassID").checked = false;
 
-  document.getElementById("goldMemberID").checked = false;
-  document.getElementById("silverMemberID").checked = false;
-  document.getElementById("greenMembeID").checked = false;
+  // document.getElementById("goldMemberID").checked = false;
+  // document.getElementById("silverMemberID").checked = false;
+  // document.getElementById("greenMembeID").checked = false;
 }
 
 function memberTypeCost() {}
